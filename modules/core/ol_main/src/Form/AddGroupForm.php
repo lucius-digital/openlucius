12117a9d75b9a60936fb7c74463103e86737073d
Joris Snoek <joris@lucius.digital>
2021-01-22 14:17:04 +0100

    Hotfix: 'OlTasks' service shouldn't be injected into 'AddGroupForm'

--- modules/core/ol_main/src/Form/AddGroupForm.php
+++ modules/core/ol_main/src/Form/AddGroupForm.php
@@ -25,11 +25,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 class AddGroupForm extends FormBase {
 
   /**
-   * @var $tasks
-   */
-  protected $tasks;
-
-  /**
    * @var $groups
    */
   protected $groups;
@@ -39,9 +34,8 @@ class AddGroupForm extends FormBase {
    *
    * @param \Drupal\ol_main\Services\OlGroups $groups
    */
-  public function __construct(OlGroups $groups, OlTasks $tasks) {
+  public function __construct(OlGroups $groups) {
     $this->groups = $groups;
-    $this->tasks = $tasks;
   }
 
   /**
@@ -49,8 +43,7 @@ class AddGroupForm extends FormBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('olmain.groups'),
-      $container->get('olboard.tasks')
+      $container->get('olmain.groups')
     );
   }
 
